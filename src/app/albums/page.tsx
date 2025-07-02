73b744144a6ce77ad8fda1ecc736b7ae45d5d24c
NIRAVDEV <101976669+NIRAVDEV@users.noreply.github.com>
2025-07-02 18:20:55 +0530

    Update page.tsx

--- src/app/albums/page.tsx
+++ src/app/albums/page.tsx
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from 'cloudinary';
+import cloudinary from '@/lib/cloudinary';
 import { AlbumCard } from "./album-card";
 
 export type Folder = { name: string; path: string };
