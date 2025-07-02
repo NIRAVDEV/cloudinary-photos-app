ad8ad9dbd0e8570a41725bccdc3d47e29a9a32bb
NIRAVDEV <101976669+NIRAVDEV@users.noreply.github.com>
2025-07-02 18:21:22 +0530

    Update page.tsx

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from 'cloudinary';
+import cloudinary from '@/lib/cloudinary';
 import { SearchResult } from "../gallery/page";
 import { ForceRefresh } from "@/components/force-refresh";
 import FavoritesList from "./favorites-list";
